640c8b19e7e8e96ceb5feb1a1e1fb56fb1d15757
Rushaniya94 <151129754+Rushaniya94@users.noreply.github.com>
2024-01-25 17:22:29 +0600

    Update 27 Rushaniya.py

--- 27 Rushaniya.py
+++ 27 Rushaniya.py
@@ -1,32 +1,72 @@
 import sqlite3
-conn= sqlite3.connect('mydatabase.db')
 
-cursor=conn.cursor()
+conn= sqlite3.connect('example.db')
 
-create_table_query='''
+testCursor= conn.cursor()
 
-INSERT INTO  employees (first_name, last_name, job_title)
+first=('''
+CREATE TABLE IF NOT EXISTS orders(
+    order_id   INT PRIMARY KEY,
+    order_name TEXT,
+    quantity INTEGER    
+)
+''')
 
-VALUES ('John', 'Jackson','Software Developer'),
-('Bob','Williams','Back-end Developer'),
-('Max','Miller','Designer');
-'''
-select_data_query='SELECT*FROM Employees;'
+second=('''
+CREATE TABLE IF NOT EXISTS employees (
+    employees_id   INT PRIMARY KEY,
+    name TEXT,
+    last_name TEXT,
+    salary INTEGER    
+)      
+       
+''')
 
-cursor.execute(select_data_query)
-rows=cursor.fetchall()
+third=('''
+CREATE TABLE IF NOT EXISTS product(
+    product_id   INT PRIMARY KEY,
+    product_name TEXT,
+    category INTEGER    
+)       
+       
+''')
 
+insert_first=('''
+INSERT OR IGNORE orders( order_id, product_name, quantity)
+VALUES
+(12345, 'laptop', 20), 
+(67890, 'computer', 10),
+(10202,'phone', 30);          
+              
+              ''')
+
+insert_second=('''
+INSERT OR IGNORE employees( employeeID, name, last_name, salary)
+VALUES
+(1, 'A','E', 20), 
+(2, 'B','R', 10),
+(3,'C','W', 30),
+(4,'D','T', 40);                                        
+              ''')
+
+insert_third=('''
+INSERT OR IGNORE products( product_id, product_name,category)
+VALUES
+(1, 'Apple','fruit'), 
+(2, 'Banana','fruit'),
+(3,'Pear','fruit'),
+(4,'Grapes', 'fruit');                                        
+              ''')
+
+testCursor.execute(first)
+testCursor.execute(second)
+testCursor.execute(third)
+testCursor.execute(insert_first)
+testCursor.execute(insert_second)
+testCursor.execute(insert_third)
+
+select_first="SELECT * FROM orders"
+testCursor.execute(select_first)
+rows=testCursor.fetchall()
 for row in rows:
     print(row)
-
-employees= session.query(Employee).all()
-
-for person in employees:
-    print(f"The employee is {person.id}, the name is {person.name} and the age is {person.age}")
-
-INSERT into employees (salary)
-SELECT salary
-
-FROM TemporaryEmployees;
-
-session.commit()
