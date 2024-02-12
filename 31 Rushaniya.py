fe2b3857bbf9461a697674ecb61a8c968dc3131a
Rushaniya94 <151129754+Rushaniya94@users.noreply.github.com>
2024-02-12 19:17:42 +0600

    Update 31 Rushaniya.py

--- 31 Rushaniya.py
+++ 31 Rushaniya.py
@@ -13,4 +13,43 @@ def filters_csv(csv_file_path):
             if row(['Name']).startwith('A'):
                 print(row)
 csv_file_path='data.csv'
-filters_csv(csv_file_path)
+filters_csv(csv_file_path
+
+import csv
+
+
+def read_books():
+    books = []
+    with open('books.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            books.append(row)
+    return books
+
+
+def add_book(books, title, author, year):
+    new_book = [title, author, year]
+    books.append(new_book)
+
+
+def save_books(books):
+    with open('books.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(books)
+
+
+existing_books = read_books()
+
+
+print("The books we have:")
+for book in existing_books:
+    print(book)
+
+
+add_book(existing_books, "The new book", "The new author", "2022")
+
+save_books(existing_books)
+
+print("The updated list:")
+for book in existing_books:
+    print(book)
