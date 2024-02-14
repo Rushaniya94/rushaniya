4de90aacf0c7cd3d9d208fb56c858d01e80f036c
Rushaniya94 <151129754+Rushaniya94@users.noreply.github.com>
2024-02-14 11:40:44 +0600

    Add files via upload

---
+++ 33 Rushaniya.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+array1 = np.array([[1, 2, 3], [4,5,6]])
+array2 = np.array([2, 4, 6])
+
+result = array1 + array2
+
+result = array1 * array2
+
+print(result)
+
+result = np.power(array1, array2)
+
+print(result)
+
+array = np.array([10, 20, 30])
+scalar = 40
+
+result = array / scalar
+
+print(result)
+
